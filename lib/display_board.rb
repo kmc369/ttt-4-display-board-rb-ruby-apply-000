3eb67afaa2ef13985ed9a1fadb95d54f1495d5eb
keisha coleman <keishacoleman710@gmail.com>
2020-09-13 02:32:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
+def display_board(board, value)
 puts "  "+board[0]+"|"+board[1]+" |"+board[2]+"   "
 puts "-----------"
 puts "  "+board[3]+"|"+board[4]+" |"+board[5]+"   "
@@ -7,4 +7,4 @@ puts "-----------"
 puts "  "+board[6]+"|"+board[7]+" |"+board[8]+"   "
 end
 
-display_board(board)
+display_board(board[0], X)
